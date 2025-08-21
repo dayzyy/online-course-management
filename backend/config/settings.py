bde385bbf542cafd2b0052c0fdfac7b389ae3a89
Luka Mania <manialuka19@gmail.com>
2025-08-21 18:11:34 +0400

    fix(backend): update settings to use `config` module instead of `backend`

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -58,7 +58,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
