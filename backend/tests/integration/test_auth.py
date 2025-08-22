75cf353497e4ee18a95008780340fe36dd3e5bb9
Luka Mania <manialuka19@gmail.com>
2025-08-23 00:38:30 +0400

    test(auth): add `get_tokens` helper

--- backend/tests/integration/test_auth.py
+++ backend/tests/integration/test_auth.py
@@ -2,18 +2,19 @@ from django.urls import reverse
 from domain.models import CustomUser
 from rest_framework.test import APIClient
 
-def test_custom_user_can_obtain_jwt_tokens(api_client: APIClient, default_user: CustomUser, default_user_password: str):
+def get_tokens(api_client: APIClient, email, password):
     path = reverse('token_obtain_pair')
-    response = api_client.post(path, {"email": default_user.email, "password": default_user_password}, format="json")
+    return api_client.post(path, {"email": email, "password": password}, format="json")
+
+def test_custom_user_can_obtain_jwt_tokens(api_client: APIClient, default_user: CustomUser, default_user_password: str):
+    response = get_tokens(api_client, default_user.email, default_user_password)
 
     assert response.status_code == 200
     assert "access" in response.data
     assert "refresh" in response.data
 
 def test_custom_user_can_refresh_token(api_client: APIClient, default_user: CustomUser, default_user_password: str):
-    path = reverse('token_obtain_pair')
-    response = api_client.post(path, {"email": default_user.email, "password": default_user_password}, format="json")
-
+    response = get_tokens(api_client, default_user.email, default_user_password)
     refresh_token = response.data['refresh']
 
     path = reverse('token_refresh')
