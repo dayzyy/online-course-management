cdd701ecbedab852f3ab791749e72f0b8a5a4147
Luka Mania <manialuka19@gmail.com>
2025-08-24 23:59:58 +0400

    feat: add `CanManageLecture` permission class

---
+++ backend/api/permissions/lecture.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission
+from domain.models import Lecture, CustomUser
+
+class CanManageLecture(BasePermission):
+    access_permitted_roles = [CustomUser.Roles.TEACHER]
+    modify_permitted_roles = []  
+
+    def has_permission(self, request, view) -> bool:
+        return any(request.user.role == role.value for role in )
+
+    def has_object_permission(self, request, view, obj: Lecture) -> bool:
+        user = request.user
+
+        if user == obj.course.lead:
+            return True
+        if user == obj.teacher:
+            return True
+        if any(user.role == role.value for role in self.modify_permitted_roles):
+            return True
+
+        return False
