901023309606c7775e5b91b00583de6dcfc6de35
Luka Mania <manialuka19@gmail.com>
2025-08-25 01:38:41 +0400

    feat: add `CanManageHomework` permission class

--- backend/api/permissions/homework.py
+++ backend/api/permissions/homework.py
@@ -1,19 +1,19 @@
 from rest_framework.permissions import BasePermission
-from domain.models import Lecture, CustomUser
+from domain.models import Homework, CustomUser
 
-class CanManageLecture(BasePermission):
+class CanManageHomework(BasePermission):
     access_permitted_roles: list[CustomUser.Roles] = [CustomUser.Roles.TEACHER]
-    modify_permitted_roles: list[CustomUser.Roles] = []  
+    modify_permitted_roles: list[CustomUser.Roles] = []
 
     def has_permission(self, request, view) -> bool:
         return any(request.user.role == role.value for role in self.access_permitted_roles + self.modify_permitted_roles)
 
-    def has_object_permission(self, request, view, obj: Lecture) -> bool:
+    def has_object_permission(self, request, view, obj: Homework) -> bool:
         user = request.user
 
-        if user == obj.course.lead:
+        if user == obj.lecture.teacher:
             return True
-        if user == obj.teacher:
+        if user == obj.lecture.course.lead:
             return True
         if any(user.role == role.value for role in self.modify_permitted_roles):
             return True
